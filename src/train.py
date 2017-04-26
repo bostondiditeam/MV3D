fdddafe3be5da66c6663d9d60d58fb44f3aaa629
XueFengZeng <t-ink@qq.com>
2017-04-26 23:24:58 +0800

    Add training testset and validation dataset config.

--- src/train.py
+++ src/train.py
@@ -7,11 +7,19 @@ import utils.batch_loading as ub
 dataset_dir = cfg.PREPROCESSED_DATA_SETS_DIR
 
 
-dates_to_drivers = {'1': ['15']}
-training = ub.batch_loading(dataset_dir, dates_to_drivers)
+training_dataset = {
+    '1': ['6_f','9_f','15','19','20'],
+    '2': ['3_f','13'],
+    '3': ['2_f','4','6','8','14','15_f']
+                    }
+training = ub.batch_loading(dataset_dir, training_dataset)
 
-dates_to_drivers = {'1': ['15']}
-validation = ub.batch_loading(dataset_dir, dates_to_drivers)
+validation_dataset = {
+    '1': ['11','21_f'],
+    '2': ['14_f','17'],
+    '3': ['7','11_f']
+}
+validation = ub.batch_loading(dataset_dir, validation_dataset)
 
 m3=mod.MV3D()
 m3.train(max_iter=10000, pre_trained=True,train_set=training,validation_set=validation)
