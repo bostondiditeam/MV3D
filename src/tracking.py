1c518b05e500706fe99903ff9c9f50fdbae39d93
XueFengZeng <t-ink@qq.com>
2017-04-27 00:03:56 +0800

    Use new dataset loader.

--- src/tracking.py
+++ src/tracking.py
@@ -7,39 +7,35 @@ import argparse
 import os
 from config import cfg
 import time
+import utils.batch_loading as ub
 
 # Set true if you want score after export predicted tracklet xml
 # set false if you just want to export tracklet xml
 
-def pred_and_save(tracklet_pred_dir, dates, drivers, frames_index=None):
+def pred_and_save(tracklet_pred_dir, dataset):
     # Tracklet_saver will check whether the file already exists.
     tracklet = Tracklet_saver(tracklet_pred_dir)
 
-    data_seg = cfg.PREPROCESSED_DATA_SETS_DIR
 
-    file_names_list = sorted(data.get_all_file_names(data_seg, dates, drivers))
-    if frames_index != None:
-        file_names_list = [file_names_list[i] for i in frames_index]
-
-    rgbs, tops, fronts, _, _ = data.load([file_names_list[0]],is_testset=True)
+    top_shape, front_shape, rgb_shape=dataset.get_shape()
     m3=mod.MV3D()
-    m3.tracking_init(tops[0].shape,fronts[0].shape,rgbs[0].shape)
+    m3.tracking_init(top_shape,front_shape,rgb_shape)
 
-    for i in range(len(file_names_list)):
-        rgb, top, front, _, _ = data.load([file_names_list[i]], is_testset=True)
+    for i in range(dataset.size):
+        rgb, top, front, _, _ = dataset.load( is_testset=True)
         t1=time.time()
         boxes3d,probs=m3.tacking(top[0],front[0],rgb[0])
         t2=time.time()
         print('time= '+ str(t2-t1))
 
         # for debugging: save image and show image.
-        top_image=data.getTopImages([file_names_list[i]])[0]
+        top_image=data.draw_top_image(top)
         if len(boxes3d)!=0:
             top_image = data.draw_box3d_on_top(top_image, boxes3d[0:1,:,:], color=(0, 0, 80))
             translation, size, rotation = boxes3d_for_evaluation(boxes3d[0:1,:,:])
             for j in range(len(translation)):
                 tracklet.add_tracklet(i, size[j]*2, translation[j], rotation[j])
-        imsave(file_names_list[i],top_image)
+        imsave('%5d_testset_top_image'%i,top_image)
 
         # file_name='tacking_test_img_{}'.format(i)
         # img_tracking=draw_boxed3d_to_rgb(rgbs[0],boxes3d)
@@ -63,8 +59,11 @@ if __name__ == '__main__':
     dates = ['Round1Test']
     drivers = ['19_f2']
     frames_index = None
+
+    dataset_loader = ub.batch_loading(cfg.PREPROCESSED_DATA_SETS_DIR, {'Round1Test': ['19_f2'],},is_testset=True)
+
     # generate tracklet file
-    pred_file = pred_and_save(tracklet_pred_dir, dates,drivers,frames_index)
+    pred_file = pred_and_save(tracklet_pred_dir, dataset_loader)
     if_score = False
 
     if(if_score):
