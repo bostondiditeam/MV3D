ba78685b78fdae7754b67e88cc133c1cdf820c51
Jing Chen <yeschenjing@gmail.com>
2017-05-11 10:28:29 +0800

    Update make.sh
    make soft links for complied .so files.

--- src/make.sh
+++ src/make.sh
@@ -3,8 +3,8 @@ python ./setup.py build_ext --inplace
 ./make.sh
 cd ../../
 
-#mv ./net/lib/roi_pooling_layer/roi_pooling.so ./net/roipooling_op/
-#mv ./net/lib/nms/gpu_nms.cpython-35m-x86_64-linux-gnu.so ./net/processing/
-#mv ./net/lib/nms/cpu_nms.cpython-35m-x86_64-linux-gnu.so ./net/processing/
-#mv ./net/lib/utils/cython_bbox.cpython-35m-x86_64-linux-gnu.so ./net/processing
+ln -s ./net/lib/roi_pooling_layer/roi_pooling.so ./net/roipooling_op/roi_pooling.so
+ln -s ./net/lib/nms/gpu_nms.cpython-35m-x86_64-linux-gnu.so ./net/processing/gpu_nms.cpython-35m-x86_64-linux-gnu.so
+ln -s ./net/lib/nms/cpu_nms.cpython-35m-x86_64-linux-gnu.so ./net/processing/cpu_nms.cpython-35m-x86_64-linux-gnu.so
+ln -s ./net/lib/utils/cython_bbox.cpython-35m-x86_64-linux-gnu.so ./net/processing/cython_bbox.cpython-35m-x86_64-linux-gnu.so
 
