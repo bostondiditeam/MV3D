89441f87c416438096584b8f733a00e3ac13abf8
Patrick Poon <patrickmpoon@gmail.com>
2017-05-11 02:38:05 +0000

    With Lance and XueFeng's help, got to point could run train.py

--- src/clear.sh
+++ src/clear.sh
@@ -7,7 +7,7 @@ rm ./net/lib/pycocotools/_mask.cpython-35m-x86_64-linux-gnu.so
 #.c
 rm ./net/lib/nms/cpu_nms.c
 rm ./net/lib/utils/bbox.c
-
+#rm ./net/lib/pycocotools/maskApi.c
 #.cpp
 rm ./net/lib/nms/gpu_nms.cpp
 
