e497cc0b01c377109df13f0670a82e3b49173fd2
XueFengZeng <t-ink@qq.com>
2017-06-06 23:37:55 +0800

    Wrap run task function.

--- src/task.py
+++ src/task.py
@@ -9,63 +9,49 @@ def copy_weigths(dir):
     os.system('cp {} {} -r'.format(weigth_path, dir))
     print('copy weigths done')
 
-def task1():
-    """
-    train image_feature and fusion net (use rpn net pretrained weights )
-    :return:
-    """
-    delta_time=0
 
-    #task 1
-    try_max=3
-    try_count=0
-    while delta_time<120 and try_count<=try_max:
-        start_time=time.time()
-        os.system('python train.py -w "top_view_rpn" -t "image_feature,fusion" -i 2000')
-        delta_time=time.time()-start_time
-        print('\n\ntraining finished ,detal time : {} retry: {}'.format(delta_time,try_count) )
-        time.sleep(2)
-        os.system('python tracking.py')
+
+def run_task(command, time_threshold=120):
+
+    delta_time = 0
+
+    # task 1
+    try_max = 3
+    try_count = 0
+    while delta_time < time_threshold and try_count <= try_max:
+        start_time = time.time()
+        os.system(command)
+        delta_time = time.time() - start_time
+        print('\n\n{} finished ,detal time : {} retry: {}'.format(command,delta_time, try_count))
         time.sleep(2)
         try_count += 1
 #
 
 def train_rpn():
-    """
-    Only train top_view_rpn net (not use pretrained weights )
-    :return:
-    """
-    delta_time=0
 
-    #task 1
-    try_max=3
-    try_count=0
-    while delta_time<120 and try_count<=try_max:
-        start_time=time.time()
-        os.system('python train.py -t "top_view_rpn" -i 2000')
-        time.sleep(2)
-        delta_time=time.time()-start_time
-        print('\n\ntraining finished ,detal time : {} retry: {}'.format(delta_time,try_count) )
+    run_task('python train.py -t "top_view_rpn" -i 600')
+    for i in range(20):
+        run_task('python train.py -w "top_view_rpn" -t "top_view_rpn" -i 600')
+        run_task('python tracking.py',time_threshold=10)
 
-        os.system('python tracking.py')
-        time.sleep(2)
-        try_count += 1
+
+
+def train_img_and_fusion():
+    run_task('python train.py -w "top_view_rpn" -t "image_feature,fusion" -i 700')
+    for i in range(20):
+        run_task('python train.py -w "top_view_rpn,image_feature,fusion" -t "image_feature,fusion" -i 600')
+        run_task('python tracking.py',time_threshold=10)
+
+
 
 def main():
 
     task_num =1
 
     if task_num == 0:
-        # tain all
         train_rpn()
-        for i in range(20):
-            task1()
-            time.sleep(5)
     elif task_num == 1:
-        # train imfeature and fusion net
-        for i in range(20):
-            task1()
-            time.sleep(5)
+        train_img_and_fusion()
 
 if __name__ == '__main__':
     main()
