3a8be7a064e3cc96500f2352518d7a4066624b06
XueFengZeng <t-ink@qq.com>
2017-06-09 22:59:12 +0800

    Remove build dir.

--- utils/bag_to_kitti/lidar/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/prerit/DiDi/MV3D/utils/bag_to_kitti/lidar/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
