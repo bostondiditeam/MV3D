1c56169921aa1426f1b00a761f1bb720608a7581
Raymond Chen <raymond.chen@gapp.nthu.edu.tw>
2017-06-11 14:00:01 +0800

    Remove legacy version of lidar_data_preprocess_cpp_wrapper.

--- src/lidar_data_preprocess_cpp_wrapper/lidar_preprocess.hpp
+++
@@ -1,5 +0,0 @@
-#include <vector>
-#include <boost/python.hpp>
-#include <python2.7/Python.h>
-
-PyObject* lidar_preprocess(const char* str);//int argc, char** argv);
--- src/lidar_data_preprocess_cpp_wrapper/lidar_preprocess_ext.cpp
+++
@@ -1,8 +0,0 @@
-/* velo_ext.cpp */
-#include "lidar_preprocess.hpp"
-
-BOOST_PYTHON_MODULE(lidar_preprocess_ext)
-{
-    using namespace boost::python;
-    def("lidar_preprocess", lidar_preprocess);
-}
