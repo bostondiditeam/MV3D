cf177335fbd036d982dd0469059a4d58fcafe498
XueFengZeng <t-ink@qq.com>
2017-06-28 17:10:39 +0800

    Remove duplicate libs about bbox_nms.

--- src/make.sh
+++ src/make.sh
@@ -2,8 +2,3 @@ cd ./net/lib/
 python ./setup.py build_ext --inplace
 ./make.sh
 cd ../../
-
-ln -s ./net/lib/nms/gpu_nms.cpython-35m-x86_64-linux-gnu.so ./net/processing/gpu_nms.cpython-35m-x86_64-linux-gnu.so
-ln -s ./net/lib/nms/cpu_nms.cpython-35m-x86_64-linux-gnu.so ./net/processing/cpu_nms.cpython-35m-x86_64-linux-gnu.so
-ln -s ./net/lib/utils/cython_bbox.cpython-35m-x86_64-linux-gnu.so ./net/processing/cython_bbox.cpython-35m-x86_64-linux-gnu.so
-
