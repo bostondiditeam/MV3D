e529301a812d1be2323e1f68cadfabb7616bb1d0
preritj <prerit.jaiswal@gmail.com>
2017-07-02 21:59:09 -0400

    improved Pedestrain Kalman

--- utils/kalman/pedestrian/filter.py
+++ utils/kalman/pedestrian/filter.py
@@ -6,9 +6,11 @@ from parse_tracklet import parse_xml
 from generate_tracklet import Tracklet, TrackletCollection
 from sort import Sort
 
-bag_file = '/media/prerit/Data/didi_data/ped/ped_test_003/003.bag'
-tracklet_file = '/media/prerit/Data/didi_data/ped/ped_test_003/003.xml'
-new_tracklet_file = '/media/prerit/Data/didi_data/ped/ped_test_003/003_corrected.xml' 
+
+tag="004"
+bag_file = '/media/prerit/Data/didi_data/ped/ped_test_'+tag+'/'+tag+'.bag'
+tracklet_file = '/media/prerit/Data/didi_data/ped/ped_test_'+tag+'/'+tag+'.xml'
+new_tracklet_file = '/media/prerit/Data/didi_data/ped/ped_test_'+tag+'/'+tag+'_corrected.xml' 
 
 
 tracklets = parse_xml(tracklet_file)
@@ -22,18 +24,21 @@ for track in tracklets:
     np.concatenate((track.trans[0],track.size))) # (x,y,z,h,w,l)
 
 
-mot_tracker = Sort(max_age=3, min_hits=5, iou_threshold=0.1, max_time_elapsed=2)
+mot_tracker = Sort(max_age=3, min_hits=6, iou_threshold=0.1, max_time_elapsed=2)
+min_detections = 7
+
 collection = TrackletCollection()
 
 
 tracklets = {}
 frame_count=0
+track_count = {}
 for t,d in zip(timestamps,detections):
     #print("detections : ", d)
     tracks =  mot_tracker.update(np.array(d),t)
     print(tracks)
     for track in tracks :    
-        trk_id = int(track[5])
+        trk_id = int(track[5]) 
         if trk_id not in tracklets.keys() :
             tracklets[trk_id] = Tracklet(
                     object_type = 'Pedestrian', 
@@ -41,6 +46,11 @@ for t,d in zip(timestamps,detections):
                     w = 2*track[3], 
                     h = track[4], 
                     first_frame=frame_count)
+            track_count[trk_id] = 0
+        else :
+            track_count[trk_id] += 1 
+        if track_count[trk_id] < min_detections :
+            continue
         frame = dict(tx=track[0],
                  ty=track[1],
                  tz=track[2],
